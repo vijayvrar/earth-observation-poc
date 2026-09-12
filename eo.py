0171cab7a75129094434a52c98ba5998a2f911c3
vijayvrar <83647741+vijayvrar@users.noreply.github.com>
2026-09-12 23:50:25 +0530

    Refactor get_satellite_image function

--- eo.py
+++ eo.py
@@ -8,7 +8,7 @@ from rasterio.warp import transform
 def get_satellite_image(latitude, longitude):
 
     # 1. Login
-   auth = earthaccess.login(strategy="environment")
+    auth = earthaccess.login(strategy="environment")
 
     if not auth.authenticated:
         raise Exception("NASA Earthdata authentication failed.")
